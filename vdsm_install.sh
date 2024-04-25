3f363145313fb54613c4dbdac318c93f110bee63
thiagoinfo <thiagofperes@gmail.com>
2024-04-25 10:21:53 -0300

    Update vdsm_install.sh

--- vdsm_install.sh
+++ vdsm_install.sh
@@ -73,41 +73,38 @@ down_host_bin(){
 boot_system_img(){
     log "$RED_INS Enter the path to virtual dsm" "$RED_INS Insira o caminho para o virtual dsm"
     read -p "default/padrao [$VDSM_DIR]: " vdsm_dir
-    if [ ! -z "$vdsm_dir" ]; then
-        VDSM_DIR=$vdsm_dir
-	VDSM_TMP_DIR=$vdsm_dir/tmp
-    fi
-	
+    [[ ! -z "$vdsm_dir" ]] && VDSM_DIR=$vdsm_dir && VDSM_TMP_DIR=$vdsm_dir/tmp
+
     log "Creating folders..." "Criando pastas..."
     mkdir -p "$VDSM_TMP_DIR"
     
     ### wget .pat from synology site
     log "Downloading $PAT_VERSION, please wait..." "Baixando $PAT_VERSION, aguarde..."
-    down_pat
-	
+    down_pat	
     [ ! -s "$VDSM_TMP_DIR/$PAT_NAME" ] && display_error_and_exit "The PAT file not found" "O arquivo PAT nao foi encontrado"
+
     log "Preparing $VDSM_BOOT_FILE$VDSM_DISK_EXT file..." "Preparando o arquivo $VDSM_BOOT_FILE$VDSM_DISK_EXT..."
     tar xpf "$VDSM_TMP_DIR/$PAT_NAME" -C "$VDSM_TMP_DIR/."
     VDSM_BOOT=$(find "$VDSM_TMP_DIR" -name "*.bin.zip")
     [ ! -s "$VDSM_BOOT" ] && display_error_and_exit "The PAT file contains no boot image." "O arquivo PAT nao contem a imagem de boot"
-    log "opkg install unzip..."
-    opkg install unzip
+
+    install_pkg "unzip" "opkg install unzip..."
+
     VDSM_BOOT=$(echo "$VDSM_BOOT" | head -c -5)
     log "Extract $VDSM_BOOT_FILE..." "Extraindo $VDSM_BOOT_FILE..."
     unzip -q -o "$VDSM_BOOT".zip -d "$VDSM_TMP_DIR"
     log "Move $VDSM_BOOT_FILE to permanent directory..." "Movendo $VDSM_BOOT_FILE para a pasta permanente..."
     VDSM_BOOT=$(find "$VDSM_TMP_DIR" -name "*.bin")
     mv $VDSM_BOOT $VDSM_DIR/$VDSM_BOOT_FILE$VDSM_DISK_EXT
-
     log "Download $VDSM_CFG_TMP..." "Baixando $VDSM_CFG_TMP..." && down_cfg
     [ ! -s "$VDSM_TMP_DIR/$VDSM_CFG_TMP" ] && display_error_and_exit "The $VDSM_CFG_TMP file not found" "O arquivo $VDSM_CFG_TMP nao foi encontrado"
     log "Save port $VDSM_PORT_SOCKET to cfg..." "Salvando porta $VDSM_PORT_SOCKET para cfg..."
     sed -i "s/port = .*/port = \"$VDSM_PORT_SOCKET\"/" $VDSM_TMP_DIR/$VDSM_CFG_TMP
     log "Save $VDSM_BOOT_FILE$VDSM_DISK_EXT to cfg..." "Salvando $VDSM_BOOT_FILE$VDSM_DISK_EXT para cfg..."
     disk_cfg "$VDSM_BOOT_FILE" "$VDSM_DIR" "0xa"
+
     ### create system disk
-    log "opkg install qemu-img"
-    opkg install qemu-img
+    install_pkg "qemu-img" "opkg install qemu-img..."
     log "Create $VDSM_SYSTEM_FILE$VDSM_DISK_EXT with size of ${VDSM_SYSTEM_SIZE}B..." "Criando $VDSM_SYSTEM_FILE$VDSM_DISK_EXT com o tamanho de ${VDSM_SYSTEM_SIZE}B..."
     qemu-img create -f raw $VDSM_DIR/$VDSM_SYSTEM_FILE$VDSM_DISK_EXT $VDSM_SYSTEM_SIZE
     [ ! -s "$VDSM_DIR/$VDSM_SYSTEM_FILE$VDSM_DISK_EXT" ] && display_error_and_exit "The $VDSM_SYSTEM_FILE$VDSM_DISK_EXT file not found" "O arquivo $VDSM_SYSTEM_FILE$VDSM_DISK_EXT nao foi encontrado"
@@ -158,11 +155,9 @@ create_net(){
     net_mac=00:60:2f$(hexdump -n3 -e '/1 ":%02x"' /dev/random)
     log "$RED_INS Enter name of the bridge interface $net_dev" "$RED_INS Insira o nome da ponte para a interface $net_dev"
     read -p "default/padrao [$VDSM_BRIDGE]: " vdsm_bridge
-    if [ -z "$vdsm_bridge" ]; then
-        vdsm_bridge=$VDSM_BRIDGE
-    fi
+    [[ -z "$vdsm_bridge" ]] && vdsm_bridge=$VDSM_BRIDGE
     bridge_check=$(ip addr | grep $vdsm_bridge)
-    [ -z "$bridge_check" ] && display_error_and_exit "Bridge $vdsm_bridge not found" "Ponte $vdsm_bridge nao encontrada"
+    [[ -z "$bridge_check" ]] && display_error_and_exit "Bridge $vdsm_bridge not found" "Ponte $vdsm_bridge nao encontrada"
     log "Save $net_dev to cfg..." "Salvando $net_dev para cfg..."
     net_cfg "$net_dev" "$dev_net" "$net_mac" "$vdsm_bridge"
 }
@@ -179,24 +174,25 @@ create_nets(){
 }
 
 create_disk(){
+
     disk_name=$1
     disk_addr=$2
     disk_file=$disk_name$VDSM_DISK_EXT
+
     log "$RED_INS Enter the path to $disk_name" "$RED_INS Insira o caminho para o $disk_name"
     read -p "default/padrao [$VDSM_DIR]: " vdsm_disk_path
-    if [ -z "$vdsm_disk_path" ]; then
-        vdsm_disk_path=$VDSM_DIR
-    fi
+    [[ -z "$vdsm_disk_path" ]] && vdsm_disk_path=$VDSM_DIR
+
     log "$RED_INS Enter $disk_name size" "$RED_INS Insira o tamanho do $disk_name"
     read -p "default/padrao [$VDSM_DISK_SIZE]: " vdsm_disk_size
-    if [ -z "$vdsm_disk_size" ]; then
-        vdsm_disk_size=$VDSM_DISK_SIZE
-    fi
+    [[ -z "$vdsm_disk_size" ]] && vdsm_disk_size=$VDSM_DISK_SIZE
+
     log "Create $disk_name with $vdsm_disk_size..." "Criando $disk_name com $vdsm_disk_size..."
     mkdir -p "$vdsm_disk_path"
     drive_file=$vdsm_disk_path/$disk_file
     qemu-img create -f raw $drive_file $vdsm_disk_size
     [ ! -s "$drive_file" ] && display_error_and_exit "The $disk_file file not found" "O arquivo $disk_file nao foi encontrado"
+
     log "Save $disk_file to cfg..." "Salvando $disk_file para cfg..."
     disk_cfg "$disk_name" "$vdsm_disk_path" "$disk_addr"
 }
@@ -237,17 +233,14 @@ cpu_ram_cfg(){
 }
 
 create_configs(){
+
     log "$RED_INS Enter the number of processor cores, only numbers (example: 1/2/4)" "$RED_INS Insira a quantidade de núcleos do processador, somente numeros (exemplo: 1/2/4)"
     read -p "default/padrao [$VDSM_CPU]: " vdsm_cpu
-    if [ -z "$vdsm_cpu" ]; then
-        vdsm_cpu=$VDSM_CPU
-    fi
+    [[ -z "$vdsm_cpu" ]] && vdsm_cpu=$VDSM_CPU
+
     log "$RED_INS Enter the amount of ram memory, only numbers (example: 1048/2048/4096)" "$RED_INS Insira a quantidade de memoria RAM, somente numeros (exemplo: 1048/2048/4096)"
     read -p "default/padrao [$VDSM_RAM]: " vdsm_ram
-    if [ -z "$vdsm_ram" ]; then
-        vdsm_ram=$VDSM_RAM
-    fi
-
+    [[ -z "$vdsm_ram" ]] && vdsm_ram=$VDSM_RAM
     cpu_ram_cfg "$vdsm_cpu" "$vdsm_ram"
 
     ## remove ^M endline file cfg
@@ -255,7 +248,8 @@ create_configs(){
     log "Copy $VDSM_INIT_TMP to $VDSM_DIR..." "Copiando $VDSM_INIT_TMP para $VDSM_DIR..."
     cp $VDSM_TMP_DIR/$VDSM_CFG_TMP $VDSM_DIR/$VDSM_CFG
 
-    log "Download $VDSM_INIT_TMP..." "Baixando $VDSM_INIT_TMP..." && down_init_d
+    log "Download $VDSM_INIT_TMP..." "Baixando $VDSM_INIT_TMP..."
+    down_init_d
     [ ! -s "$VDSM_TMP_DIR/$VDSM_INIT_TMP" ] && display_error_and_exit "The $VDSM_INIT_TMP file not found" "O arquivo $VDSM_INIT_TMP nao foi encontrado"
 
     init_d_update "$vdsm_cpu"
@@ -288,61 +282,55 @@ init_d_update(){
     VDSM_CPU=$1
     VDSM_DIR_E=$(echo "${VDSM_DIR//\//\\/}")
 
-    log "Update $VDSM_INIT_TMP script..." "Atualizando $VDSM_INIT_TMP script..."
-
     init_h_mac=s/VDSM_H_MAC=.*/VDSM_H_MAC=\"$VDSM_H_MAC\"/
-    sed -i "$init_h_mac" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_h_model=s/VDSM_H_MODEL=.*/VDSM_H_MODEL=\"$VDSM_H_MODEL\"/
-    sed -i "$init_h_model" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_h_hostsn=s/VDSM_H_HOSTSN=.*/VDSM_H_HOSTSN=\"$VDSM_H_HOSTSN\"/
-    sed -i "$init_h_hostsn" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_h_guestsn=s/VDSM_H_GUESTSN=.*/VDSM_H_GUESTSN=\"$VDSM_H_GUESTSN\"/
-    sed -i "$init_h_guestsn" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_name=s/VDSM_NAME=.*/VDSM_NAME=\"$VDSM_NAME\"/
-    sed -i "$init_name" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_dir=s/VDSM_DIR=.*/VDSM_DIR=\"$VDSM_DIR_E\"/
-    sed -i "$init_dir" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_cpu=s/VDSM_H_CPU=.*/VDSM_H_CPU=$VDSM_CPU/
-    sed -i "$init_cpu" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_api_port=s/VDSM_H_API_PORT=.*/VDSM_H_API_PORT=$VDSM_H_API_PORT/
-    sed -i "$init_api_port" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     init_socket_port=s/VDSM_PORT_SOCKET=.*/VDSM_PORT_SOCKET=$VDSM_PORT_SOCKET/
-    sed -i "$init_socket_port" $VDSM_TMP_DIR/$VDSM_INIT_TMP
-
     VDSM_H_CPU_ARCH=$(cat /proc/cpuinfo | grep 'model name' | cut -f 2 -d ":" | awk '{$1=$1}1' | sed 's# @.*##g' | sed s/"(R)"//g | sed 's/[^[:alnum:] ]\+/ /g' | sed 's/  */ /g' | head -1)
     init_cpu_model=s/VDSM_H_CPU_ARCH=.*/VDSM_H_CPU_ARCH=\"$VDSM_H_CPU_ARCH,,\"/
-    sed -i "$init_cpu_model" $VDSM_TMP_DIR/$VDSM_INIT_TMP
+
+    log "Update $VDSM_INIT_TMP script..." "Atualizando $VDSM_INIT_TMP script..."
+    
+    sed -i \
+      -e "$init_h_mac" \
+      -e "$init_h_model" \
+      -e "$init_h_hostsn" \
+      -e "$init_h_guestsn" \
+      -e "$init_name" \
+      -e "$init_dir" \
+      -e "$init_cpu" \
+      -e "$init_api_port" \
+      -e "$init_socket_port" \
+      -e "$init_cpu_model" \
+      $VDSM_TMP_DIR/$VDSM_INIT_TMP
+
+}
+
+install_pkg(){
+    log "$2"
+    opkg install $1
 }
 
 install_required_pkg(){
-    log "opkg install curl..."
-    opkg install curl
 
-    log "opkg install kmod-tun qemu-bridge-helper qemu-x86_64-softmmu ..."
-    opkg install kmod-tun qemu-bridge-helper qemu-x86_64-softmmu
+    install_pkg "curl" "opkg install curl ..."    
+    install_pkg "kmod-tun qemu-bridge-helper qemu-x86_64-softmmu" "opkg install kmod-tun qemu-bridge-helper qemu-x86_64-softmmu ..."
 
     ### intel
     is_intel=$(cat /proc/cpuinfo | grep GenuineIntel)
-    if [ ! -z "$is_intel" ]; then
-        log "opkg install kmod-kvm-intel intel-microcode iucode-tool ..."
-        opkg install kmod-kvm-intel intel-microcode iucode-tool
-    fi
+    [[ ! -z "$is_intel" ]] && install_pkg "kmod-kvm-intel intel-microcode iucode-tool" "opkg install kmod-kvm-intel intel-microcode iucode-tool ..."
 
     ### amd
     is_amd=$(cat /proc/cpuinfo | grep AuthenticAMD)
-    if [ ! -z "$is_amd" ]; then
-        log "opkg install kmod-kvm-amd amd64-microcode ..."
-        opkg install kmod-kvm-amd amd64-microcode
-    fi
+    [[ ! -z "$is_amd" ]] && install_pkg " kmod-kvm-amd amd64-microcode" "opkg install kmod-kvm-amd amd64-microcode ..."
 }
+
+
 
 delete_temp(){
     log "Delete temp folder and files..." "Excluindo pasta e arquivos temporarios..."
